22db60dfe5167277a14f08a7d3ddad0d23c7b980
BrayanSolanoF <36710897+BrayanSolanoF@users.noreply.github.com>
2018-08-21 09:06:51 -0600

    Add files via upload

---
+++ Modulacion_operandos.py
@@ -0,0 +1,11 @@
+def op(A,B,OP): #Modulo principal
+    if OP == 1
+        Res = sum(A,B)
+        return  Res
+    elif OP == 2
+        Res = res(A,B)
+        return Res
+    elif OP == 3
+        return Res
+        Res = mult(A,B)
+    else: return "Error en operaciòn: argumento OP"
---
+++ quiz multip imaginarios.py
@@ -0,0 +1,10 @@
+def mult_comp(Real1,Imag1,Real2,Imag2):
+    """
+    Entradas: 2 numeros reales y 2 numeros complejos
+    Salidas: multiplicacion de numeros complejos y numeros reales(tupla)
+    Version 1.0
+    Autor Stephanie Canales Cerdas
+    """
+    multip = complex(Real1,Imag1)*complex(Real2,Imag2)
+    return multip
+    
