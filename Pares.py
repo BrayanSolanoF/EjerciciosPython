4459a327d462cbc14021ccc23f34996504121969
BrayanSolanoF <36710897+BrayanSolanoF@users.noreply.github.com>
2018-09-04 16:34:57 -0600

    Add files via upload

---
+++ Pares.py
@@ -0,0 +1,22 @@
+"""
+-Programa: fun Det_numeros_pares
+-Entradas: Numeros enteros en forma de lista
+-Salidas: Nueva lista con numeros pares de la lista anterior
+-Restricciones: Ingresar valores en forma de lista
+-Version:1.0
+"""
+
+
+def pares(lista):
+    if not isinstance(lista,list)or lista==[]:
+        return None
+    else:
+        return pares_aux(lista,[])
+
+def pares_aux(lista,lista2):
+    if lista==[]:
+        return lista2
+    if lista[0]%2==0:
+        return pares_aux(lista[1:], lista2+lista[0])
+    else: pares_aux(lista[1:], lista2)
+        
